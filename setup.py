380358acd5b1f406221fab598cedcec60dc4f453
kandarpa02 <kandarpaexe@gmail.com>
2025-09-07 12:06:56 +0530

    added a training script, requirements file as reqs.txt

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages # type: ignore 
 
 setup(
-    name="DeepTranslate",
+    name="DotProdAttention",
     version="0.1.0",
     author="Kandarpa Sarkar",
     author_email="kandarpaexe@gmail.com",
     description="Machine translation model based on 2017 paper by google 'Attention is all you need'",
     long_description=open("README.md").read(),
-    url="https://github.com/kandarpa02/DeepTranslate.git",
+    url="https://github.com/kandarpa02/DotProdAttention.git",
     packages=find_packages(),
     python_requires=">=3.8",
     classifiers=[
